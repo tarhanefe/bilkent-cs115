113f1e36f31847dbfc1aa02ab10224ac12523ca3
TurinTurambarX <efe.tarhan@ug.bilkent.edu.tr>
2022-06-27 11:45:30 +0300

    Bilkent University CS115 Labs

---
+++ Lablar/Lab 9/doctor.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 11 13:16:12 2020
+
+@author: user
+"""
+
+class Doctor:
+    
+    def __init__(self, dId, dName, dSpec, hosp):
+        self.__doctorId = dId
+        self.__doctorName = dName
+        self.__specialty = dSpec
+        self.__hospital = hosp
+
+    def get_doctor_id(self):
+        return self.__doctorId
+    
+    
+    def set_doctor_id(self, dId):
+        self.__doctorId = dId
+
+    def get_doctor_name(self):
+        return self.__doctorName
+
+    def set_doctor_name(self, dName):
+        self.__doctorName = dName
+    
+    def get_specialty(self):
+        return self.__specialty
+    
+    def set_specialty(self, dSpec):
+        self.__specialty = dSpec
+
+    def get_hospital(self):
+        return self.__hospital
+
+    def set_hospital(self, hosp):
+        self.__hospital= hosp
+    
+    def __repr__(self):
+        return self.__doctorId+' '+self.__doctorName+' '+self.__specialty+' '+self.__hospital
+    
+    def __lt__(self,other):
+        return self.__doctorId < other.__doctorId
+    
+    
+        
+            
